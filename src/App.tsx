eef020b3aaf283d12b8b8f67663ee715b8c229da
Ben Feist <benjamin.f.feist@nasa.gov>
2026-02-21 16:55:21 -0500

    Add vite react template front-end

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App(): React.JSX.Element {
+  return (
+    <div>
+      <h1>Ask Anything</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: "0.0.0.0",
+    port: 9300,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      components: path.resolve(__dirname, "./src/components"),
+      assets: path.resolve(__dirname, "./src/assets"),
+      styles: path.resolve(__dirname, "./src/styles"),
+    },
+  },
+});
